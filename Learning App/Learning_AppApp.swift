bf2f8dcbd64665342462d9248f766a66c940c2e5
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-02-12 23:36:26 -0500

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Howard Thomas on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
